35c2ae66ae8f4fe734d898dcda0c5e7c2589595e
Lucas Santos Souza <48518972+a2104563@users.noreply.github.com>
2021-02-26 09:23:56 -0300

    Update SelectionSort.c

--- SelectionSort.c
+++ SelectionSort.c
@@ -1,33 +1,38 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-void bubblesort( int *V, int X){
-	int aux , fim, N = X;
-	while(fim != 0){	
-		for(int i = 0; i < N-1; i++){
-			if(V[i] > V[i+1]){
-				aux = V[i];
-				V[i] = V[i+1];
-				V[i+1] = aux;
-				fim = i;
-			}
-		}
-		N = N-1;
-	}
+void selectionSort(int *V, int n){
+    int menor, aux, i, j = 0;
+    int fim, indice;
+    
+    fim= n;
+    
+    for(i=0; i<fim; i++){
+        menor= i;
+        for(j=i; j<fim; j++){
+            if(V[menor]>V[j]){
+                menor =j;
+            }
+        }
+        
+        if(i != menor){
+            aux = V[i];
+            V[i]= V[menor];
+            V[menor]= aux;
+        }
+    }
 }
+  
 
-
-int main() {
-    // Write C code here
- int Teste[10] = {9,8,7,6,5,4,3,2,1,0};
-    for (int i = 0; i < 10; i++){
-        printf("%i",Teste[i]);
+int main(){
+    
+    int vetor [10] = {7,4,1,0,8,5,2,9,6,3};
+    
+    selectionSort(vetor,10);
+    
+    for (size_t i= 0;i<10; i++){
+        printf("%i", vetor[i]);
     }
 
-    bubblesort(Teste,10);
-
-    printf("\n");
-
-    for (int i = 0; i < 10; i++){
-        printf("%i",Teste[i]);
-    }
+  return 0;
 }
